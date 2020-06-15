bf70e4bdb4d5e59ec5c000a8d8c47f9f353ca33c
Ian McElroy <mcelroyian@gmail.com>
2020-06-15 16:52:16 -0500

    init

---
+++ schools/src/main/java/com/lambdaschool/schools/exceptions/ResourceFoundException.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.schools.exceptions;
+
+/**
+ * A custom exception to be used when a resource is found but is not suppose to be
+ */
+public class ResourceFoundException
+        extends RuntimeException
+{
+    public ResourceFoundException(String message)
+    {
+        super("Found an issue with School " + message);
+    }
+}
